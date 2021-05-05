083f8b66e370aca4b809a5599c7e0e7d33cc8bbb
Martin Ramirez <mramirez7@uc.cl>
2021-05-04 23:41:40 -0400

    tsp

--- TSP.cpp
+++ TSP.cpp
@@ -1,69 +1,53 @@
-#pragma GCC optimize("Ofast")
+#pragma adj_matrixCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ff               first
-#define ss               second
-#define pb               push_back
-#define all(x)           (x).begin(), (x).end()
-#define mem0(x)          memset(x, 0, sizeof (x))
-#define mem1(x)          memset(x, -1, sizeof (x))
+const int MAXN = 16, INF = 1e9;
+int dp_table[MAXN][1<<MAXN];
+int adj_matrix[MAXN][MAXN];
+int nodes;
 
-typedef long long ll;
-#define int ll
-typedef vector <int> vi;
-typedef vector<vi> vvi;
-typedef pair<int, int> ii;
-typedef vector <ii> vii;
-typedef vector<vii> vvii;
-
-const int MAXN = 16, INF = 1e17;
-int memo[MAXN][1<<MAXN];
-int G[MAXN][MAXN];
-int n;
-
-int dp(int city, int mask){
-    if (!mask) return memo[city][mask] = 0;
-    if (~memo[city][mask]) return memo[city][mask];
+int dp(int city, int cities_visited_mask){
+    if (!cities_visited_mask) return dp_table[city][cities_visited_mask] = 0;
+    if (~dp_table[city][cities_visited_mask]) return dp_table[city][cities_visited_mask];
     int ans = INF;
-    for (int i = 0; i < n; ++i) {
-        if ((1<<i)&mask) ans = min(ans, G[city][i] + dp(i, mask ^ (1<<i)));
+    for (int i = 0; i < nodes; ++i) {
+        if ((1<<i)&cities_visited_mask) ans = min(ans, adj_matrix[city][i] + dp(i, cities_visited_mask ^ (1<<i)));
     }
-    return memo[city][mask] = ans;
+    return dp_table[city][cities_visited_mask] = ans;
 }
 
-void recovery(int city, int mask, int val, string &s){
-    if (!mask) return;
-    for (int i = 0; i < n; ++i) {
-        if (((1<<i)&mask) && val ==  G[city][i] + dp(i, mask ^ (1<<i))){
-            s += (i+'A');
-            recovery(i,  mask^(1<<i), val - G[city][i], s);
+void recovery(int city, int cities_visited_mask, int val, string &answer_path){
+    if (!cities_visited_mask) return;
+    for (int i = 0; i < nodes; ++i) {
+        if (((1<<i)&cities_visited_mask) && val ==  adj_matrix[city][i] + dp(i, cities_visited_mask ^ (1<<i))){
+            answer_path += (i+'A');
+            recovery(i,  cities_visited_mask^(1<<i), val - adj_matrix[city][i], answer_path);
             return;
         }
     }
 }
 
-string find_best(vvi &A){
-    n = A.size();
-    for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) G[i][j] = A[i][j];
-    mem1(memo);
+string find_best(vector<vector<int>> &A){
+    nodes = A.size();
+    for (int i = 0; i < nodes; ++i) for (int j = 0; j < nodes; ++j) adj_matrix[i][j] = A[i][j] == -1 ? INF : A[i][j];
+    memset(dp_table, -1, sizeof(dp_table));
     int min_cost = INF;
     int start = -1;
     for (int i = 0; i < 1; ++i) {
-        if (dp(i, ((1<<n)-1)^(1<<i))  < min_cost) min_cost = dp(i, ((1<<n)-1)^(1<<i)), start = i;
+        if (dp(i, ((1<<nodes)-1)^(1<<i))  < min_cost) min_cost = dp(i, ((1<<nodes)-1)^(1<<i)), start = i;
     }
     if (min_cost == INF){
         return "NO EXISTE CAMINO\n";
     }
-    string s; s += start+'A';
-    recovery(start, ((1<<n)-1)^(1<<start),  min_cost, s);
-    cout << s << "\n";
-    return s;
+    string answer_path; answer_path += start+'A';
+    recovery(start, ((1<<nodes)-1)^(1<<start),  min_cost, answer_path);
+    return answer_path;
 }
 
 int32_t main(){
-    int n, m; cin >> n >> m;
-    vvi A(n, vi(n, INF));
+    int m; cin >> nodes >> m;
+    vector<vector<int>> A(nodes, vector<int>(nodes, INF));
     for (int i = 0; i < m; ++i) {
         int w;
         char u, v; cin >> u >> v >> w;
